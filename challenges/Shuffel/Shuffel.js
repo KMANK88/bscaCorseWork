db797c580a5423183cc6cb961dbfa57e7a4d2bd0
Karsten Pease <karstenpease88@outlook.com>
2017-06-23 11:36:45 -0600

    newer shuffel

--- challenges/Shuffel/Shuffel.js
+++ challenges/Shuffel/Shuffel.js
@@ -1,6 +1,5 @@
 
 
-
 function generateDeck(){
 
 var handDelt = [];
@@ -8,10 +7,49 @@ var handDelt = [];
   for(var i=1; i<= 52; i+=1){
 
     handDelt.push(i);
+
   }return handDelt;
 }
+
+
+function deckMap(deckIn){
+    for(var i=0; i<deckIn.length;i+=1){
+
+      var card = ' ';
+
+    if(deckIn[i] <=13){
+
+      deckIn[i]= deckIn[i] + 'C';
+
+    }else if(deckIn[i]<=26){
+
+            //conditions        if true                  or false
+        deckIn[i] = (deckIn[i]%13 !==0) ? (deckIn[i]%13) + 'H' : '13H';
+
+    }else if (deckIn[i]<= 39){
+
+    deckIn[i] = (deckIn[i]%13 !==0) ? (deckIn[i]%13) + 'D' : '13D';
+
+  }else if (deckIn[i]<=52){
+
+      deckIn[i] = (deckIn[i]%13 !==0) ? (deckIn[i]%13) + 'S' : '13S';
+
+
+  }else {
+
+    console.log("To many cards");
+
+    }
+  }
+return deckIn
+
+}
+
+
 var deck = generateDeck();
-//console.log(deck);
+
+deckMap(deck);
+console.log("Mapped deck:" , deck)
 
 function shuffle(deckIn) {
   for(var j =0; j<deckIn.length; j+=1){
@@ -36,4 +74,4 @@ deck.forEach(function(card,index){
     checkArray.push(card);
   }
 });
-console.log(checkArray.length);
+//console.log(checkArray.length);
