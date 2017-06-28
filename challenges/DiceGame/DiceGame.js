1bdfa754f056f3a2a357643a7a1133bd69ef095f
Karsten Pease <karstenpease88@outlook.com>
2017-06-28 10:43:48 -0600

    updated

--- challenges/DiceGame/DiceGame.js
+++ challenges/DiceGame/DiceGame.js
@@ -11,7 +11,7 @@ function checker (){
   if((roll1.d1 + roll1.d2) === 7 ||(roll1.d1 + roll1.d2) === 11){
     console.log("I win ", roll1.d1,roll1.d2);
   }else {
-     console.log("I didnt win Di" + roll1.d1, roll1.d2)
+     console.log("I didnt win Di " + roll1.d1, roll1.d2)
    }
 }
     checker();
